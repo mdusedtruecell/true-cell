28dc8e27d81655eb6bca46dd787dc91e5d5086ff
Mohd Used <mohdusedtruecell@gmail.com>
2026-06-29 18:40:03 +0400

    update sales rep api

--- src/api/salesRepApi.ts
+++ src/api/salesRepApi.ts
@@ -5,10 +5,14 @@ export interface SalesRep {
 }
 
 export const SALES_REPS: SalesRep[] = [
-    { id: 1, name: 'Shahid Khalid', code: 'TC0050' },
-    { id: 2, name: 'Ahmed', code: 'TC0663' },
-    { id: 3, name: 'John Doe', code: 'TC0777' },
     { id: 3, name: 'Atif Pardesi', code: 'TC0777' },
+    { id: 1, name: 'Shahid Khalid', code: 'TC0050' },
+    { id: 2, name: 'M Usaid', code: 'TC0663' },
+    { id: 3, name: 'Shazen Khan', code: 'TC0777' },
+    { id: 3, name: 'Faisal Ahmed', code: 'TC0777' },
+    { id: 3, name: 'Talha Amdani', code: 'TC0777' },
+    { id: 3, name: 'Humaira Abdul Ghani', code: 'TC0777' },
+    { id: 3, name: 'Hamadou Mounkaila', code: 'TC0777' },
 ];
 
 export const fetchSalesReps = async (): Promise<SalesRep[]> => {
